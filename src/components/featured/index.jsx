da81775c69d3a910d9d2acac7062636c65339a9f
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-11-22 22:22:37 +0100

    feat: Featured section

---
+++ src/components/featured/index.jsx
@@ -0,0 +1,12 @@
+import { Carousel } from "./Carousel";
+
+export const Featured = () => {
+  return (
+    <div className="featured_container">
+      <Carousel />
+      <div className="artist_name">
+        <div className="wrapper">Ariana grande</div>
+      </div>
+    </div>
+  );
+};
